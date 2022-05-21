2d25d0407aef8a8da9d1f95f124b1b6d52c0475e
Tuấn Hoàn <ntanh.hutech@gmail.com>
2022-05-21 23:06:42 +0700

    score

--- angular-13/src/app/material-component/Admin/score-view/score-item/score-item.component.ts
+++ angular-13/src/app/material-component/Admin/score-view/score-item/score-item.component.ts
@@ -9,6 +9,7 @@ import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 })
 export class ScoreItemComponent implements OnInit {
   @Input() SemesterId = 0;
+  @Input() StudentId = localStorage.getItem("studentId");
   displayedColumns: string[] = ["subject", "15P", "60P", "HK", "TB"];
   dataSource: any;
   scores: Score[] = [];
@@ -16,42 +17,35 @@ export class ScoreItemComponent implements OnInit {
   constructor(private httpService: HttpServerService) {}
   ngOnInit(): void {
     this.httpService.Get("Semesters").subscribe((data) => {
-      console.log(data);
-
       this.semester = data;
     });
 
-    this.httpService
-      .Get("Students/current-student?userId=" + localStorage.getItem("userId"))
-      .subscribe((student) => {
-        this.httpService.Get("Subject").subscribe((subject) => {
-          this.httpService
-            .Get(
-              "Scores/student/" + student.id + "/Semester/" + this.SemesterId
-            )
-            .subscribe((data) => {
-              subject.forEach((element: any) => {
-                var socreSb = data.filter(
-                  (x: any) => x.subjectId == element.id
-                );
-                var score = new Score();
-                score.score15p = socreSb[0]?.point;
-                score.score60p = socreSb[1]?.point;
-                score.scoreHK = socreSb[2]?.point;
-                score.subject = element.name;
-                score.scoreTb = +(
-                  (socreSb[0]?.point +
-                    socreSb[1]?.point * 2 +
-                    socreSb[2]?.point * 3) /
-                  6
-                ).toFixed(2);
-                this.scores.push(score);
-              });
-              console.log(this.scores);
-              this.dataSource = this.scores;
-            });
+    this.httpService.Get("Subject").subscribe((subject) => {
+      this.httpService
+        .Get(
+          "Scores/student/" + this.StudentId + "/Semester/" + this.SemesterId
+        )
+        .subscribe((data) => {
+          subject.forEach((element: any) => {
+            var socreSb = data.filter((x: any) => x.subjectId == element.id);
+            var score = new Score();
+            score.score15p = socreSb[0]?.point;
+            score.score60p = socreSb[1]?.point;
+            score.scoreHK = socreSb[2]?.point;
+            score.subject = element.name;
+            score.scoreTb = +(
+              (socreSb[0]?.point +
+                socreSb[1]?.point * 2 +
+                socreSb[2]?.point * 3) /
+              6
+            ).toFixed(2);
+            this.scores.push(score);
+          });
+          this.dataSource = this.scores;
         });
-      });
+    });
+  }
+  DbClick(){
   }
 }
 
